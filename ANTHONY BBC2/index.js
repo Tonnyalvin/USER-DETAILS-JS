70e52d028780435718134a153b340748ffc5a629
Tonnyalvin <149709991+Tonnyalvin@users.noreply.github.com>
2024-02-07 05:27:03 -0800

    Add files via upload

---
+++ ANTHONY BBC2/index.js
@@ -0,0 +1,8 @@
+function correctCrediations(email, password){
+    if(email=="alvinturibamwealvin@gmail.com" && password==2200704806){
+        console.log(`login SucceSful`);
+    }else {
+        console.log(`incorrect email or password`);
+    }
+}
+    correctCrediations("alvinturibamwealvin@gmail.com",2200704806);
